acc5741452a05b50400be34a67b7058278ab40eb
Jim Rollenhagen <jim@jimrollenhagen.com>
2014-02-27 23:20:14 -0800

    more skeleton

--- control/lib/listener.js
+++ control/lib/listener.js
@@ -14,6 +14,22 @@ Listener.prototype.listen = function() {
 
 Listener.prototype.handle = function(subChannel, channel, data) {
   console.log(channel + ': ' + data);
+  var pieces = channel.split(':');
+  if (pieces.length !== 3) {
+    // bail
+    return;
+  }
+
+  command = pieces[2];
+
+  if (command === 'create') {
+    var message = JSON.parse(data);
+    // download python file etc
+  } else if (command === 'tick') {
+    // run a function
+    // zvsh --zvm-image python.tar python @something.py args moreargs
+    // then push stdout back to redis
+  }
 };
 
 exports.Listener = Listener;

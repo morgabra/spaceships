17d316b2c94d87d9d590b13c9e8a832355751daf
Jim Rollenhagen <jim@jimrollenhagen.com>
2014-02-28 11:01:06 -0800

    controller: call out to zerovm and stuff

--- control/lib/listener.js
+++ control/lib/listener.js
@@ -1,17 +1,17 @@
+var exec = require('child_process').exec;
 var fs = require('fs');
 var redis = require('redis');
 var request = require('request');
 
 var Listener = function() {
-  this.redis = redis.createClient();
-  this.redis.debug_mode = true;
-
+  this.subscriber = redis.createClient();
+  this.publisher = redis.createClient();
 };
 
 Listener.prototype.listen = function() {
-  this.redis.on('pmessage', this.handle);
+  this.subscriber.on('pmessage', this.handle.bind(this));
 
-  this.redis.psubscribe('api:*');
+  this.subscriber.psubscribe('api:*');
 };
 
 Listener.prototype.handle = function(subChannel, channel, data) {
@@ -36,10 +36,22 @@ Listener.prototype.handle = function(subChannel, channel, data) {
       // already exists
     }
     request(url).pipe(fs.createWriteStream(directory + '/firmware.py'));
+
   } else if (command === 'tick') {
     // run a function
     // zvsh --zvm-image python.tar python @something.py args moreargs
     // then push stdout back to redis
+    var firmware = '/tmp/' + username + '/firmware.py';
+    var shellCommand = 'zvsh --zvm-image /home/vagrant/tarball/python.tar python @' + firmware + ' ' + data;
+    var that = this;
+    var child = exec(shellCommand, function(error, stdout, stderr) {
+      if (error) {
+        // handle it
+      }
+
+      var pubChannel = 'api:' + username + ':result';
+      that.publisher.publish(pubChannel, stdout);
+    });
   }
 };
 

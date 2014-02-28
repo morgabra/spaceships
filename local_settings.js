27b63f53edbadc2677a12d9c8e6c0465a3b0d78c
Brad Morgan <bradley.morgan@rackspace.com>
2014-02-27 19:45:34 -0600

    initial commit, github auth

---
+++ local_settings.js
@@ -0,0 +1,25 @@
+var config = {
+
+  static_path: 'static',
+
+  listen_port: 3000,
+
+  github: {
+    id: 'bba70812257735390d7a',
+    secret: '2efad22faba7d1c0986e50c928299753563f8d83'
+  },
+
+  db: {
+    db: 'spaceships',
+    host: 'localhost'
+    //port: 27017,  // optional, default: 27017
+    //username: 'admin', // optional
+    //password: 'secret', // optional
+    //collection: 'mySessions' // optional, default: sessions
+  },
+
+  // session secret - change this
+  secret: 'ziifos7moiwe1eeghee4sha3uut3doh6eW9zaiquahshooTeh7'
+};
+
+module.exports = config;

8c85f749df0a666a5e8181c0f87dcb339c658c09
Lars Butler <Lars.Butler@gmail.com>
2014-02-28 10:57:29 -0600

    Add drawing for spaceships
    Includes rotation, position, and labels. We're almost ready to pipe in
    live data from the world backend.

--- static/lib/app.js
+++ static/lib/app.js
@@ -1,47 +1,14 @@
 var framerate;
 var canvas;
 var graphics;
-var ship;
-
-function main() {
-    framerate = new Framerate("framerate-monitor");
-    canvas = document.getElementById("gamecanvas");
-    graphics = canvas.getContext("2d");
-    // FIXME(larsbutler): this is a hack because the canvas is stretched,
-    // and I don't know why
-    graphics.scale(1.0, 0.7);
-
-    ship = new Image();
-    ship.src = "media/ObjectSpaceship.png";
-
-    var gameLoop = function() {
-        update();
-
-        graphics.fillStyle = "rgba(0, 0, 0, 1.0)";
-        graphics.clearRect(0, 0, canvas.width, canvas.height);
-        graphics.fillStyle = "rgba(240, 150, 10, 1.0)";
-        graphics.drawImage(ship, 16, 16);
-        graphics.fillStyle = "rgba(0, 0, 150, 1.0)";
-        framerate.snapshot();
-        window.requestAnimationFrame(gameLoop);
-    };
-
-    window.requestAnimationFrame(gameLoop);
-
-}
-
-
-function update() {
-    // Do stuff to update the game
-}
-
+var shipImage;
 // Data received here looks something like this:
-/*
 var testData = [
   {
-    x: 12, // coord
+    name: 'larsbutler',
+    x: 50, // coord
     y: 123, // coord
-    heading: 10, //degrees
+    heading: 15, //degrees
     speed: 100, // units/second
     scanners: [
       // list of objects within view
@@ -50,7 +17,7 @@ var testData = [
         y: 111,
         name: 'zeries120',
         distance: 800, // units
-        direction: 98, // degrees from 0 on the coord plane
+        heading: 98, // degrees from 0 on the coord plane
         type: 'planet',
         size: 78 // area of bounding box
       },
@@ -59,7 +26,7 @@ var testData = [
         y: 87,
         name: 'jirwin',
         distance: 800, // units
-        direction: 98, // degrees from 0 on the coord plane
+        heading: 98, // degrees from 0 on the coord plane
         type: 'ship',
         shield: 65, // percentage of shield charge,
         health: 100, // hit points
@@ -86,17 +53,74 @@ var testData = [
     ]
   }
 ];
-*/
+
+var me = testData[0];
+
+function main() {
+    framerate = new Framerate("framerate-monitor");
+    canvas = document.getElementById("gamecanvas");
+    graphics = canvas.getContext("2d");
+
+    shipImage = new Image();
+    shipImage.src = "media/ObjectSpaceship.png";
+
+    var gameLoop = function() {
+        // Time?
+        update();
+        render(graphics);
+        framerate.snapshot();
+        window.requestAnimationFrame(gameLoop);
+    };
+
+    window.requestAnimationFrame(gameLoop);
+
+}
+
+function render(graphics) {
+    // Clear
+    graphics.fillStyle = "rgba(0, 0, 0, 1.0)";
+    graphics.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw my ship
+    // loop over other ships and draw them
+    // cyan text
+    drawShip(graphics, me, shipImage, "rgba(0, 255, 255, 1.0)");
+    for (var i = 0; i < me.scanners.length; i++) {
+        var eachShip = me.scanners[i];
+        // white text
+        drawShip(graphics, eachShip, shipImage, "rgba(255, 255, 255, 1.0)");
+    }
+}
+
+function drawShip(graphics, shipObj, image, textRgba) {
+    var shipWidth = 64;
+    var shipHeight = 64;
+
+    graphics.save();
+    // graphics.translate(canvas.width/2, canvas.height/2);
+    graphics.translate(shipObj.x, shipObj.y);
+    graphics.rotate(shipObj.heading * Math.PI/180);
+    graphics.drawImage(image, -image.width/2, -image.width/2);
+    graphics.restore();
+    graphics.fillStyle = textRgba;
+    graphics.fillText(shipObj.name, shipObj.x - 32, shipObj.y + 32);
+}
+
+
+function update() {
+    // Do stuff to update the game
+}
+
 
 $( document ).ready( function () {
     main();
-	var socket = io.connect('http://localhost');
+    var socket = io.connect('http://localhost');
 
-	socket.on('data', function (data) {
-  		console.log(data.data);
-  		console.log($('#shipstatus'));
-  		$('#shipstatus').html(data.data);
-  		socket.emit('data', { my: 'data' });
-	});
+    socket.on('data', function (data) {
+        console.log(data.data);
+        console.log($('#shipstatus'));
+        $('#shipstatus').html(data.data);
+        socket.emit('data', { my: 'data' });
+    });
 });
 

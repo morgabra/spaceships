a52a0564beb3ba1e1974b0827b8dc0e2e0c28fd9
Lars Butler <Lars.Butler@gmail.com>
2014-02-28 14:22:59 -0600

    static/lib/app: super gnarly ship rendering

--- static/lib/app.js
+++ static/lib/app.js
@@ -2,61 +2,9 @@ var framerate;
 var canvas;
 var graphics;
 var shipImage;
-var tickData = {ship: null};// is updated by the socketio stuff below
+var tickData = {ships: null};// is updated by the socketio stuff below
+var tickCounter = 0;
 
-// Data received here looks something like this:
-var testData = [
-  {
-    name: 'larsbutler',
-    x: 50, // coord
-    y: 123, // coord
-    heading: 15, //degrees
-    speed: 100, // units/second
-    scanners: [
-      // list of objects within view
-      {
-        x: 220,
-        y: 111,
-        name: 'zeries120',
-        distance: 800, // units
-        heading: 98, // degrees from 0 on the coord plane
-        type: 'planet',
-        size: 78 // area of bounding box
-      },
-      {
-        x: 270,
-        y: 87,
-        name: 'jirwin',
-        distance: 800, // units
-        heading: 98, // degrees from 0 on the coord plane
-        type: 'ship',
-        shield: 65, // percentage of shield charge,
-        health: 100, // hit points
-        size: 1 // area of bounding box
-      }
-    ],
-    sensors: {
-      engineTemperature: 900, // unbounded int
-      power: 80, // 0-100
-      shield: 99, // 0-100
-      throttle: 29, // 0-100
-    },
-    log: [
-      {
-        'type': 'combat',
-        'message': 'You were hit for 6 damage',
-        'timestamp': 213908210823 // timestamp in milliseconds
-      },
-      {
-        'type': 'combat',
-        'message': 'You were hit for 6 damage',
-        'timestamp': 213908210823 // timestamp in milliseconds
-      }
-    ]
-  }
-];
-
-var me = testData[0];
 
 function main() {
     framerate = new Framerate("framerate-monitor");
@@ -80,20 +28,21 @@ function main() {
 
 function render(graphics) {
     // Clear
-    graphics.fillStyle = "rgba(0, 0, 0, 1.0)";
-    graphics.clearRect(0, 0, canvas.width, canvas.height);
 
     // draw my ship
     // loop over other ships and draw them
     // cyan text
-    drawShip(graphics, tickData.ship, shipImage, "rgba(0, 255, 255, 1.0)");
-    /*
-    for (var i = 0; i < me.scanners.length; i++) {
-        var eachShip = me.scanners[i];
-        // white text
-        drawShip(graphics, eachShip, shipImage, "rgba(255, 255, 255, 1.0)");
+    if (tickData.ships === null) {
+        return;
     }
-    */
+
+    if (tickCounter % 30 == 0) {
+        graphics.fillStyle = "rgba(0, 0, 0, 1.0)";
+        graphics.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    for (var i = 0; i < tickData.ships.length; i++) {
+        drawShip(graphics, tickData.ships[i], shipImage, "rgba(0, 255, 255, 1.0)");
+    }
 }
 
 function drawShip(graphics, shipObj, image, textRgba) {
@@ -104,7 +53,6 @@ function drawShip(graphics, shipObj, image, textRgba) {
     var shipHeight = 64;
 
     graphics.save();
-    // graphics.translate(canvas.width/2, canvas.height/2);
     graphics.translate(shipObj.x, shipObj.y);
     graphics.rotate(shipObj.heading * Math.PI/180);
     graphics.drawImage(image, -image.width/2, -image.width/2);
@@ -116,6 +64,15 @@ function drawShip(graphics, shipObj, image, textRgba) {
 
 function update() {
     // Do stuff to update the game
+    tickCounter++;
+    if (tickCounter > 10) {
+        tickCounter = 0;
+    }
+
+    // HACK: translate the origin from top left to center
+    if (tickData.ships === null) {
+        return;
+    }
 }
 
 
@@ -131,10 +88,16 @@ $( document ).ready( function () {
             return;
         }
         // We got ship data
-        tickData.ship = JSON.parse(data.data);
+        tickData.ships = JSON.parse(data.data);
+        for (var i = 0; i < tickData.ships.length; i++) {
+            var ship = tickData.ships[i];
+            ship.x += 320;
+            ship.y += 240;
+        }
+
 
         // console.log($('#shipstatus'));
-        $('#shipstatus').html(data.data);
+        // $('#shipstatus').html(data.data);
         socket.emit('data', { my: 'data' });
     });
 });
